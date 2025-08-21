c0d5c82ac06b1a2dd37280ce3f2fc61352213302
netvincible <netvincible@gmail.com>
2025-08-21 19:18:59 +0530

    pravah 1st draft with error

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include "pravah.h"
+using namespace std;
+int main()
+{
+    pravah<int> a({3,1,3,5,2,6});
+    pravah<int> b(4);
+    pravah<int> c(5,1);
+
+    int n=a.maap();
+    for(int i=0;i<n;i++)
+    {
+        cout << a.pr[i] << " ";
+    }
+    cout << endl;
+
+    a.nivesh(3,10);
+    cout<< "insert" << endl;
+    n=a.maap();
+    for(int i=0;i<n;i++)
+    {
+        cout << a.pr[i] << " ";
+    }
+    cout << endl;
+
+    a.vinivesh(1);
+    cout<< "pop at 1" << endl;
+    n=a.maap();
+    for(int i=0;i<n;i++)
+    {
+        cout << a.pr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "index of 10: " << a.dhundho(10) << endl;
+
+    a.kram();
+    cout<< "sort" << endl;
+    n=a.maap();
+    for(int i=0;i<n;i++)
+    {
+        cout << a.pr[i] << " ";
+    }
+    cout << endl;
+
+    a.viprit();
+    cout <<"reverse" << endl;
+    n=a.maap();
+    for(int i=0;i<n;i++)
+    {
+        cout << a.pr[i] << " ";
+    }
+    cout << endl;
+
+    pravah<int> d;
+    d.nakal(a);
+    cout <<"copy a in d" <<endl;
+    n=d.maap();
+    for(int i=0;i<n;i++)
+    {
+        cout << d.pr[i] << " ";
+    }
+    cout << endl;
+
+    a.maap_parivartan(a.maap()-2);
+    cout <<"reduced size by 2" << endl;
+    n=a.maap();
+    for(int i=0;i<n;i++)
+    {
+        cout << a.pr[i] << " ";
+    }
+    cout << endl;
+
+    a.maap_parivartan(a.maap()+2);
+    cout <<"increased size by 2" << endl;
+    n=a.maap();
+    for(int i=0;i<n;i++)
+    {
+        cout << a.pr[i] << " ";
+    }
+    cout << endl;
+}
+
+/*
+New Things learned:
+    1. Template
+    2. Initializer_list
+    3. When using templete its better to write driver.cpp in header file
+*/
---
+++ pravah.h
@@ -0,0 +1,153 @@
+#ifndef PRAVAH_H
+#define PRAVAH_H
+#include <initializer_list>
+#include <type_traits>
+#include <memory>
+#include <iostream>
+using namespace std;
+template <typename T> class pravah
+{
+    private:
+        size_t sz;
+
+    public:
+        T *pr;
+        pravah()
+        {
+            pr=nullptr;
+            sz=0;
+        }
+        pravah(size_t s):sz(s)
+        {
+            T value;
+            if constexpr (is_integral<T>::value || is_floating_point<T>::value) {
+            value = 0;
+        }
+            
+            pr = new T[sz];               // new uses malloc internally
+            
+            for(int i=0;i<sz;i++)
+                pr[i]=value;
+        }
+        pravah(size_t s, T value)
+        {
+            sz=s;
+            pr=new T[sz+1];
+            for(int i=0;i<sz;i++)
+                pr[i]=value;
+        }
+        pravah(initializer_list<T> initial_list)
+        {
+            sz=initial_list.size();
+            pr=new T [sz];
+            int i=0;
+            for(auto v:initial_list)
+            {
+                pr[i]=v;
+                i++;
+            }
+        }
+        void nivesh(int index,T value)
+        {
+            T* temp=new T[sz+1];
+            for(int i=0;i<sz;i++)
+            {
+                if(i<index)
+                    temp[i]=pr[i];
+                else if(i>index)
+                    temp[i+1]=pr[i];
+                else
+                    temp[i]=value;
+            }
+            delete[] pr;
+            pr=temp;
+            sz++;
+        }
+        void vinivesh(int index)
+        {
+            T* temp=new T[sz-1];
+            for(int i=0;i<sz;i++)
+            {
+                if(i<index)
+                    temp[i]=pr[i];
+                else if(i>index)
+                    temp[i-1]=pr[i];
+            }
+            delete[] pr;
+            pr=temp;
+            sz--;
+        }
+        int dhundho(T value)
+        {
+            for(int i=0;i<sz;i++)
+            {
+                if(value==pr[i])
+                    return i;
+            }
+            return -1;
+        }
+        int maap()
+        {
+            return sz;
+        }
+        void kram()
+        {
+            for(int i=0;i<sz;i++)
+            {
+                bool flag=true;
+                for(int j=0;j<sz-i-1;j++)
+                {
+                    if(pr[j]>pr[j+1])
+                    {
+                        T temp=pr[j];
+                        pr[j]=pr[j+1];
+                        pr[j+1]=temp;
+                        flag=false;
+                    }
+                }
+                if (flag)
+                    break;
+            }
+        }
+        void viprit()
+        {
+            for(int i=0;i<(sz/2 +1);i++)
+            {
+                T temp=pr[i];
+                pr[i]=pr[sz-i-1];
+                pr[sz-i-1]=temp;
+            }
+        }
+
+        void nakal(pravah<T> obj)   //Here you can understand why do we call method of object in which value is to be copied and why the value to copied is passed as argument
+        {
+            size_t newS=obj.maap();
+            if(pr==nullptr)
+                pr=new T[newS];
+            for(int i=0;i<newS;i++)
+                pr[i]=obj.pr[i];
+            sz=newS;
+        }
+        void maap_parivartan(size_t s)
+        {
+            
+            size_t n = (s < sz) ? s : sz;
+            T *newpr;
+            newpr= new T[s];
+            for (size_t i = 0; i < n; i++)
+            {
+                newpr[i] = pr[i];
+            }
+            delete[] pr;
+            pr = newpr;
+            newpr=nullptr;
+            sz = s;
+        }
+        ~pravah()
+        {
+            
+            delete[] pr;
+            pr=nullptr;
+        }
+};
+#endif
